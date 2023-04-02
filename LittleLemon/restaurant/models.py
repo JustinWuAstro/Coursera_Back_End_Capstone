9619ba54080350ad3d1cf87690638055918865be
JustinWuAstro <33946706+JustinWuAstro@users.noreply.github.com>
2023-04-02 13:18:48 +0800

    Add models of booking and menu

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=200)
+    No_of_guests = models.SmallIntegerField()
+    BookingDate = models.DateField()
+
+    def __str__(self): 
+        return self.Name
+
+class Menu(models.Model):
+   Title = models.CharField(max_length=200)
+   Price = models.DecimalField(max_digits=10, decimal_places=2) 
+   Inventory = models.IntegerField(null=False)  
+
+   def __str__(self):
+      return self.Title
